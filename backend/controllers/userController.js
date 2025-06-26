60cbaf59f6beb22c0d3b9e77881ae7a689c5bbd6
Tony Garrett <tony.garrett@gmail.com>
2025-06-25 21:34:30 -0400

    Completed adduser controller with completed error handling for users that already exist

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import { db } from '../connect.js';
 import bcrypt from 'bcryptjs';
 
+// Function for simply testing pulling user information out of the database
+// Not for production use
 export const getUsers = (req, res, next) => {
   res.set('content-type', 'application/json');
   const sql = 'SELECT * FROM users';
@@ -33,9 +35,27 @@ export const getUsers = (req, res, next) => {
 // Requires 5 pieces of information to add a new user
 export const addUser = async (req, res, next) => {
   res.set('content-type', 'application/json');
+
+  // Query the database to determine if a user with the username or email already exists
+  const usernameTest = `SELECT * FROM Users WHERE username = ?`;
+  const emailTest = `SELECT * FROM Users WHERE email = ?`;
+
+  if (await fetchFirst(db, usernameTest, req.body.username)) {
+    const error = new Error(`An account already exists with that username`);
+    error.status = 400;
+    return next(error);
+  }
+
+  if (await fetchFirst(db, emailTest, req.body.email)) {
+    const error = new Error(
+      `An account already exists with that email address`
+    );
+    error.status = 400;
+    return next(error);
+  }
+
   const sql =
     'INSERT INTO users(username, password, email, firstName, lastName) VALUES(?, ?, ?, ?, ?)';
-
   try {
     db.run(
       sql,
@@ -43,26 +63,24 @@ export const addUser = async (req, res, next) => {
         req.body.username,
         await hashPassword(req.body.password),
         req.body.email,
-        req.body.firstName,
-        req.body.lastName,
+        req.body.firstName || 'none',
+        req.body.lastName || 'none',
       ],
       function (err) {
         if (err) throw err;
         res.status(201);
-        let data = { status: 201, message: `User ${req.body.firstName}` };
+        let data = {
+          status: 201,
+          message: `User ${req.body.firstName} ${req.body.lastName}`,
+        };
         let content = JSON.stringify(data);
         res.send(content);
       }
     );
-  } catch (err) {
-    if (err === 'SQLITE_CONSTRAINT') {
-      // Check for unique constraint violation
-      res
-        .status(409)
-        .json({ error: 'Duplicate entry: unique value already exists' }); // 409 Conflict
-    } else {
-      next(err); // Pass other errors to the global error handler
-    }
+  } catch (error) {
+    error.message = 'Error adding the new user.';
+    error.status = 400;
+    return next(error);
   }
 };
 
@@ -103,8 +121,28 @@ async function hashPassword(password) {
     const salt = await bcrypt.genSalt(saltRounds);
     const hash = await bcrypt.hash(password, salt);
     return hash;
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     return null;
   }
 }
+
+// Wrapper functions taken directly from the sqlitetutorial.net site that make fetching from the db easier and uses promises to do it
+
+export const fetchAll = async (db, sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
+  });
+};
+
+export const fetchFirst = async (db, sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => {
+      if (err) reject(err);
+      resolve(row);
+    });
+  });
+};
